e764fe63f6c3133b6cbf4d8a4317010b7159837b
kushsingh-aa <01kushsingh@gmail.com>
2026-09-09 15:30:27 +0530

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,19 @@
+public class Overloading {
+public static void main(String[] args) {
+        Addition obj = new Addition();
+        obj.sum();
+        obj.sum(10,20);
+        obj.sum(10.1,12.1);
+    }
+}
+class Addition{
+    void sum(){
+        System.out.println("Addition ");
+    }
+    void sum(int a, int b){
+        System.out.println("Addition " +(a+b));
+    }
+    void sum(double a, double b){
+        System.out.println("Addition " +(a+b));
+    }
+}
